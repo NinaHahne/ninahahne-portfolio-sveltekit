248a6e8c4e85b65fd1717d8263c9bf3635d9fdcd
NinaHahne <ninamariehahne@gmail.com>
2025-07-29 17:54:48 +0200

    style: update responsive layout for tablet portrait

--- src/lib/components/SegmentTabs.svelte
+++ src/lib/components/SegmentTabs.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bg-light-gray-60 flex w-full flex-wrap items-center justify-center gap-4 p-4 xl:gap-8">
-  <div class="order-2 flex-1 text-left md:order-none xl:text-right">
+  <div class="order-2 flex-1 text-left lg:order-none xl:text-right">
     <button
       class="relative rounded-md border-2 border-dusty-brown px-4 py-1 text-sm font-semibold text-dusty-brown transition-colors duration-300 hoverable:hover:bg-dusty-brown hoverable:hover:text-light-gray"
       class:bg-dusty-brown={selected === 'projects'}
@@ -16,9 +16,9 @@
     </button>
   </div>
 
-  <h1 class="order-1 w-full text-center text-3xl font-bold md:order-none md:w-auto md:text-left">PORTFOLIO</h1>
+  <h1 class="order-1 w-full text-center text-3xl font-bold lg:order-none lg:w-auto lg:text-left">PORTFOLIO</h1>
 
-  <div class="order-3 flex-1 text-right md:order-none xl:text-left">
+  <div class="order-3 flex-1 text-right lg:order-none xl:text-left">
     <button
       class="relative rounded-md border-2 border-dusty-brown px-4 py-1 text-sm font-semibold text-dusty-brown transition-colors duration-300 hoverable:hover:bg-dusty-brown hoverable:hover:text-light-gray"
       class:bg-dusty-brown={selected === 'experiments'}
